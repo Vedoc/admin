54aff8cb446f6f820ec063796db64026a9b2efbc
arnoldnek@gmail.com <arnoldnek@gmail.com>
2024-06-04 12:07:23 +0300

    prepped for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,10 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
+
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV[ 'RAILS_SERVE_STATIC_FILES' ].present?
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   # config.public_file_server.enabled = false
@@ -27,7 +30,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
+  # config.assets.compile = true
   config.assets.compile = false
+  config.assets.digest = true
+
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
@@ -69,9 +75,25 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "admin_production"
+  # config.active_job.queue_name_prefix = "admin_repo_production"
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    user_name: ENV[ 'MAIL_USERNAME' ],
+    password: ENV[ 'MAIL_PASSWORD' ],
+    domain: ENV[ 'APP_HOST' ],
+    address: ENV[ 'SMTP_DOMAIN' ],
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -84,8 +106,26 @@ Rails.application.configure do
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
+  # Send deprecation notices to registered listeners.
+  config.active_support.deprecation = :notify
+
+  config.assets.precompile += %w( active_admin.scss application.css active_admin.js )
+
+    # Use default logging formatter so that PID and timestamp are not suppressed.
+    config.log_formatter = ::Logger::Formatter.new
+
+    if ENV[ 'RAILS_LOG_TO_STDOUT' ].present?
+      logger           = ActiveSupport::Logger.new( STDOUT )
+      logger.formatter = config.log_formatter
+      config.logger    = ActiveSupport::TaggedLogging.new( logger )
+    end
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  config.serve_static_assets = true
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
