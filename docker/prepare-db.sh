dcfc365fa92ce7f4b1935aaff652f7f2a6ab1ad0
arnoldnek@gmail.com <arnoldnek@gmail.com>
2024-06-07 23:43:54 +0300

    fix 2

--- docker/prepare-db.sh
+++ docker/prepare-db.sh
@@ -1,5 +1,5 @@
 # ! /bin/sh
 
 # If database exists, migrate. Otherwise, create and seed
-bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup db:seed
-echo 'Database migration for admin side and seeding completed!'
+# bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup db:seed
+# echo 'Database migration for admin side and seeding completed!'
